f4fea15f14183fdd2fdf5919ba2fbf7689239319
Joe Jones <jjones730@yahoo.com>
2023-03-22 19:04:38 -0500

    Bug fixes and improvements for slideshow control

--- docs/slideshow/slideshow.js
+++ docs/slideshow/slideshow.js
@@ -1,6 +1,8 @@
-let global_data = [];
+let global_ss_data = [];
 /*
-  global_data = [
+  global_ss_data structure: 
+
+  global_ss_data = [
     {
       description = "text", # From JSON
       header_image = "url", # From JSON
@@ -8,61 +10,86 @@ let global_data = [];
       current_index: n      # Calculated
     }
     ...
-  ]  
+  ];
 */
 
-function initSlideShow(jsonPath, containerId) {
-    let currentId = global_data.length;
-    global_data[currentId] = "Loading...";
-    renderUI(containerId, currentId);
+function initSlideShow(jsonPath, containerId, width, height) {
+    let slideShowIndex = global_ss_data.length;
+    global_ss_data[slideShowIndex] = "undefined";
     fetch(jsonPath)
         .then(response => {
-            return response.json();
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error("Unable to load slideshow json from " + jsonPath);
         })
         .then(data => {
-            global_data[currentId] = data.content;
-            global_data[currentId].current_index = 0;
-            slideTo(0, currentId);
+            global_ss_data[slideShowIndex] = data.content;
+            global_ss_data[slideShowIndex].current_index = 0;
+            renderUI(containerId, slideShowIndex, width, height);
+            preloadImages(data.content, slideShowIndex);
+            slideTo(0, slideShowIndex);
+        })
+        .catch((error) => {
+            renderError(containerId, error);
         });
 }
 
-function renderUI(containerId, slideShowId) {
+function preloadImages(slideShowData, slideShowIndex) {
+    var cache = document.getElementById("ss_cache_" + slideShowIndex);
+    for (var i = 0; i < slideShowData.length; i++) {
+        var img = new Image();
+        img.src = slideShowData[i].header_image;
+        img.style = "position:absolute";
+        cache.appendChild(img);
+    }
+}
+
+function renderError(containerId, error) {
+    var container = document.getElementById(containerId);
+    container.innerHTML = error;
+}
+
+function renderUI(containerId, slideShowIndex, width, height) {
     var container = document.getElementById(containerId);
     container.innerHTML =
-        '<div class="card">' +
-        '  <div class="header">' +
-        '    <img class="header_image" id="ss_header_image_' + slideShowId + '"></img>' +
-        '    <div class="nav_button prev_button" onclick="javascript:slidePrev(' + slideShowId + ')">' +
+        '<div class="card" style="width:' + width + ';">' +
+        '  <div class="header" style="height:' + height + ';" id="ss_header_' + slideShowIndex + '">' +
+        '    <div class="nav_button prev_button" onclick="javascript:slidePrev(' + slideShowIndex + ')">' +
         '      <p class="nav_button_label">&lt;&lt;</p>' +
         '    </div>' +
-        '    <div class="nav_button next_button" onclick="javascript:slideNext(' + slideShowId + ')">' +
+        '    <div class="nav_button next_button" onclick="javascript:slideNext(' + slideShowIndex + ')">' +
         '      <p class="nav_button_label">&gt;&gt;</p>' +
         '    </div>' +
         '  </div>' +
-        '  <div class="title" id="ss_title_' + slideShowId + '"></div>' +
-        '  <div class="description" id="ss_description_' + slideShowId + '"></div>' +
-        '</div>';
+        '  <div class="title" id="ss_title_' + slideShowIndex + '"></div>' +
+        '  <div class="description" id="ss_description_' + slideShowIndex + '"></div>' +
+        '</div>' +
+        '<div id="ss_cache_' + slideShowIndex + '" style="position:absolute;z-index:-1000;opacity:0;"></div>';
 }
 
-function slideNext(slideShowId) {
-    let curIndex = global_data[slideShowId].current_index;
-    slideTo(curIndex + 1, slideShowId);
+function slideNext(slideShowIndex) {
+    let currentIndex = global_ss_data[slideShowIndex].current_index;
+    slideTo(currentIndex + 1, slideShowIndex);
 }
 
-function slidePrev(slideShowId) {
-    let curIndex = global_data[slideShowId].current_index;
-    slideTo(curIndex - 1, slideShowId);
+function slidePrev(slideShowIndex) {
+    let currentIndex = global_ss_data[slideShowIndex].current_index;
+    slideTo(currentIndex - 1, slideShowIndex);
 }
 
-function slideTo(newIndex, slideShowId) {
-    let curIndex = newIndex;
-    if (curIndex >= global_data[slideShowId].length) { curIndex = 0; }
-    if (curIndex < 0) { curIndex = global_data[slideShowId].length - 1; }
-    global_data[slideShowId].current_index = curIndex;
+function slideTo(proposedIndex, slideShowIndex) {
+    // Get and validate new index from proposed index;
+    let newIndex = proposedIndex;
+    if (newIndex >= global_ss_data[slideShowIndex].length) { newIndex = 0; }
+    if (newIndex < 0) { newIndex = global_ss_data[slideShowIndex].length - 1; }
 
-    let content = global_data[slideShowId][curIndex];
-    console.log(global_data);
-    document.getElementById('ss_header_image_' + slideShowId).src = content.header_image;
-    document.getElementById('ss_title_' + slideShowId).innerHTML = content.title;
-    document.getElementById('ss_description_' + slideShowId).innerHTML = content.description;
+    // Cache new index in global data
+    global_ss_data[slideShowIndex].current_index = newIndex;
+
+    // Update slide with content data from new index
+    let content = global_ss_data[slideShowIndex][newIndex];
+    document.getElementById('ss_header_' + slideShowIndex).style.backgroundImage = "url('" + content.header_image + "')";
+    document.getElementById('ss_title_' + slideShowIndex).innerHTML = content.title;
+    document.getElementById('ss_description_' + slideShowIndex).innerHTML = content.description;
 }
